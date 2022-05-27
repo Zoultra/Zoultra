b8d87c136051200e5b51f391382cf0193134681c
Zou <traorezoul7@gmail.com>
2022-05-27 15:08:56 +0000

    Fonctionnalité slider

--- firstPrpject/myJsFile.js
+++ firstPrpject/myJsFile.js
@@ -1,27 +1,7 @@
-/*var images = [
-    'http://www.sololearn.com/uploads/slider/1.jpg',
-    'http://www.sololearn.com/uploads/slider/2.jpg',
-    'http://www.sololearn.com/uploads/slider/3.jpg'
-];
-var num = 0;
+/*
 
-function next() {
-    var slider = document.getElementById('slider');
-    num++;
-    if (num >= images.length) {
-        num = 0;
-    }
-    slider.src = images[num];
-}
 
-function prev() {
-    var slider = document.getElementById('slider');
-    num--;
-    if (num < 0) {
-        num = images.length - 1;
-    }
-    slider.src = images[num];
-}
+
 
 let basic = {
     ex1: 'PushUps: 20 times',
@@ -38,7 +18,7 @@ var total = Object.assign({}, basic, advanced)
 for (let ex in total) {
     console.log(total[ex])
 };*/
-
+/*
 function Add(...input) {
 
     sum = 0;
@@ -53,3 +33,29 @@ function Add(...input) {
 console.log(Add(1, 2, 3));
 console.log(Add(4, 14, 5, 9, 14));
 console.log(Add(2, 36));
+*/
+
+var images = [
+    'http://www.sololearn.com/uploads/slider/1.jpg',
+    'http://www.sololearn.com/uploads/slider/2.jpg',
+    'http://www.sololearn.com/uploads/slider/3.jpg'
+];
+var num = 0;
+
+function prev() {
+    var slider = document.getElementById('slider');
+    num--;
+    if (num < 0) {
+        num = images.length - 1;
+    }
+    slider.src = images[num];
+}
+
+function next() {
+    var slider = document.getElementById('slider');
+    num++;
+    if (num >= images.length) {
+        num = 0;
+    }
+    slider.src = images[num];
+}
